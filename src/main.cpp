b08cf7e095c5ba9889ebe63c14116d3cf0a40809
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-03 19:28:50 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 #include "Config.h"
 #include <SoftwareSerial.h>
 #include "Scheduler.hpp"
-#include "GSM.hpp"
 #include "GPS.hpp"
+#include "GSM.hpp"
 
-GSM* gsm;
 GPS* gps;
+GSM* gsm;
 
 void GpsTask(void* args)
 {
@@ -29,13 +29,14 @@ void Main()
     //This has to be set first before any other objects are initalized as if they write to serial before this the baud is set to something different.
     Serial.begin(9600);
 
-    gsm = new GSM(MODEM_TX, MODEM_RX);
-    gps = new GPS(GPS_TX, GPS_RX);
-
-    Scheduler::Add(1000, &GpsTask);
     #ifdef DEBUG
     Scheduler::Add(5000, [](void*){ Serial.println("SERIAL_ALIVE_CHECK:" + String(millis() / 1000)); }); //TODO: Testing only. Just for me to make sure the serial monitor or board hasn't frozen.
     #endif
+
+    gps = new GPS(GPS_TX, GPS_RX);
+    Scheduler::Add(1000, &GpsTask);
+
+    gsm = new GSM(MODEM_TX, MODEM_RX);
 }
 
 #ifdef ARDUINO
