faaba5997dd8a6215522905e3c062e5a3878b8de
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-03 18:40:22 +0100

    GPS

--- src/GPS.hpp
+++ src/GPS.hpp
@@ -8,47 +8,78 @@
 class GPS
 {
 private:
-    SoftwareSerial gpsSerial;
-    TinyGPSPlus tinyGps;
-    ulong updateTaskId;
+    SoftwareSerial* _gpsSerial;
+    ulong _updateTaskId;
+    size_t _bufferSize;
+    char* _serialBuffer;
+    uint _bufferIndex = 0;
+    ulong _bufferFlushInterval;
+    ulong _lastProcessTime = 0;
 
-    static void Update(void* args)
+    static void ReadSerial(void* args)
+    {
+        GPS* self = static_cast<GPS*>(args);
+
+        while (self->_gpsSerial->available())
+        {
+            if (self->_bufferIndex >= self->_bufferSize)
+                break;
+
+            char c = self->_gpsSerial->read();
+            self->_serialBuffer[self->_bufferIndex++] = c;
+            // Serial.write(c); //TODO: For testing only.
+        }
+
+        //Possibly move this to the task scheduler instead.
+        ulong now = millis();
+        if (self->_bufferIndex >= self->_bufferSize || now - self->_lastProcessTime > 1000)
+        {
+            ProcessBuffer(args);
+            self->_lastProcessTime = now;
+        }
+    }
+
+    static void ProcessBuffer(void* args)
     {
         GPS* self = static_cast<GPS*>(args);
 
         bool updated = false;
-        while (self->gpsSerial.available())
-        {
-            char c = self->gpsSerial.read();
-            updated |= self->tinyGps.encode(c);
-            Serial.write(c);
-        }
-        // if (!updated)
-        //     return;
+        for (size_t i = 0; i < self->_bufferIndex; i++)
+            updated |= self->TinyGps.encode(self->_serialBuffer[i]);
+        //No need to empty the buffer, memory is reserved so we'd just be taking up extra unnecessary cpu cycles.
+        self->_bufferIndex = 0;
 
-        // if (self->tinyGps.location.isValid())
-        // {
-            Serial.println();
-            Serial.print("Lng: ");
-            Serial.println(self->tinyGps.location.lng() , 6);
-            Serial.print("Lat: ");
-            Serial.println(self->tinyGps.location.lat() , 6);
-            Serial.print("Sat: ");
-            Serial.println(self->tinyGps.satellites.value(), 6);
-            Serial.print("Age: ");
-            Serial.println(self->tinyGps.location.age(), 6);
-        // }
     }
-
+    
 public:
-    GPS(uint8_t rxPin, uint8_t txPin, ulong updateInterval = 1000)
+    TinyGPSPlus TinyGps;
+
+    GPS(uint8_t rxPin, uint8_t txPin, ulong bufferFlushInterval = 1000)
+    //From my tests, a buffer size of 512 is adequate for 1 second of data (+ some extra). So use this as a base and scale according to the buffer flush interval.
+    : _bufferSize(512 * (bufferFlushInterval / 1000)), _bufferFlushInterval(bufferFlushInterval)
     {
-        gpsSerial.begin(9600, SWSERIAL_8N1, rxPin, txPin);
-        updateTaskId = Scheduler::Add(updateInterval, GPS::Update, this);
+        _gpsSerial = new SoftwareSerial(rxPin, txPin);
+        _gpsSerial->begin(9600);
+        
+        _serialBuffer = (char*)malloc(_bufferSize * sizeof(char));
+        if (_serialBuffer == nullptr)
+        {
+            Serial.println("Not enough memory to allocate GPS serial buffer");
+            abort();
+        }
+
+        //Setting this interval to be too slow causes data to be missed or corrupted and the sentences never get complete.
+        //Debating wether this is better than just updating in realtime. Less CPU is used per unit of time like this so it should theoretically be more energy efficient.
+        _updateTaskId = Scheduler::Add(1, GPS::ReadSerial, this);
     }
 
     ~GPS()
     {
-        Scheduler::Remove(updateTaskId);
+        Scheduler::Remove(_updateTaskId);
+
+        delete _serialBuffer;
+
+        _gpsSerial->end();
+        delete _gpsSerial;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,33 @@ void SerialTask(void* args)
         char c = modemSerial.read();
         Serial.print(c);
     }
+    // if (gpsSerial.available())
+    // {
+    //     char c = gpsSerial.read();
+    //     Serial.print(c);
+    // }
+}
+
+void GpsTask(void* args)
+{
+    if (!gps.TinyGps.location.isUpdated())
+        return;
+
+    Serial.println();
+    Serial.print("Lat: ");
+    Serial.println(gps.TinyGps.location.lat(), 6);
+    Serial.print("Lng: ");
+    Serial.println(gps.TinyGps.location.lng(), 6);
 }
 
 void Main()
 {
     Serial.begin(9600);
     modemSerial.begin(9600);
+    // gpsSerial.begin(9600);
 
     Scheduler::Add(1, &SerialTask);
+    Scheduler::Add(1000, &GpsTask);
 }
 
 #ifdef ARDUINO
