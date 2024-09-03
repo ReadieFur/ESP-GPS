9cdac6fe8669b5769e3b74dfe7452e5efcd31700
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-03 19:09:54 +0100

    Update GSM.hpp

--- src/GSM.hpp
+++ src/GSM.hpp
@@ -4,6 +4,7 @@
 #ifdef DEBUG
 #include <StreamDebugger.h>
 #endif
+#include <TinyGSM.h>
 
 class GSM
 {
@@ -16,7 +17,9 @@ private:
     #endif
 
 public:
-    GSM(uint8_t rxPin, uint8_t txPin)
+    TinyGsm* Modem;
+
+    GSM(uint8_t rxPin, uint8_t txPin, const char* pin = "")
     {
         #ifdef DEBUG
         _rawModemSerial = new SoftwareSerial(rxPin, txPin);
@@ -26,10 +29,16 @@ public:
         _modemSerial = new SoftwareSerial(rxPin, txPin);
         _modemSerial->begin(9600);
         #endif
+
+        Modem = new TinyGsm(*_modemSerial);
     }
 
     ~GSM()
     {
+        //TODO: Clean up all GSM connections here (preferably by API calls, otherwise via manual AT commands).
+        Modem->gprsDisconnect();
+        delete Modem;
+
         #ifdef DEBUG
         _rawModemSerial->end();        
         delete _rawModemSerial;
