4cc2005099a8d71cdcbf9b230553588682c61b8c
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-18 19:22:09 +0100

    Update Config.example.h

--- src/Config.example.h
+++ src/Config.example.h
@@ -7,10 +7,10 @@
 
 #define MQTT_BROKER             ""                  //MQTT broker IP or domain.
 #define MQTT_PORT               1883                //MQTT port.
+#define MQTT_CLIENT_ID          ""                  //The MQTT topic to subscribe to.
 #define MQTT_USERNAME           ""                  //MQTT Username, leave blank for none.
 #define MQTT_PASSWORD           ""                  //MQTT Password, leave blank for none.
 #define MQTT_TOPIC              ""                  //The MQTT topic base. Messages are published to MQTT_TOPIC/MQTT_CLIENT_ID/data, and the module will subscribe to MQTT_TOPIC/MQTT_CLIENT_ID/api
-#define MQTT_CLIENT_ID          ""                  //The MQTT topic to subscribe to.
 
 #define BATTERY_CRIT_SLEEP      10 * 60 * 1000      //Time in milliseconds to sleep when the battery is critically low.
 #define BATTERY_LOW_INTERVAL    30 * 60 * 1000      //How often (in milliseconds) to publish data to the MQTT broker when the battery is low.
