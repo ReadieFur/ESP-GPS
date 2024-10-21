97340fbf95a880aca6776a5e0cb9dc6c220dc9f2
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-10-21 19:47:59 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <esp_log.h>
 #include "Board.h"
 #include "Config.h"
+#include "Service/ServiceManager.hpp"
+#include "SerialMonitor.hpp"
+#include "GPS.hpp"
+#include "GSM.hpp"
+#include "Motion.hpp"
+#include "Storage.hpp"
+
+using namespace ReadieFur;
 
 void setup()
 {
+    Service::ServiceManager::InstallService<EspGps::SerialMonitor>();
+
+    EspGps::Storage::Init();
+    EspGps::Motion::Configure();
+    
+    Service::ServiceManager::InstallService<EspGps::GPS>();
+    Service::ServiceManager::InstallService<EspGps::GSM>();
+
+    Service::ServiceManager::StartService<EspGps::SerialMonitor>();
+    Service::ServiceManager::StartService<EspGps::GPS>();
+    Service::ServiceManager::StartService<EspGps::GSM>();
 }
 
 void loop()
