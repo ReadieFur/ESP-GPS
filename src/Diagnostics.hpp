823197f276f461b66b18c5842b72ce39985bd6f4
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-10-21 22:38:26 +0100

    Update Diagnostics.hpp

--- src/Diagnostics.hpp
+++ src/Diagnostics.hpp
@@ -17,10 +17,10 @@ namespace ReadieFur::EspGps
     class Diagnostics : public Service::AService
     {
     private:
-        #if configUSE_TRACE_FACILITY == 1
-        static bool GetCpuTime(std::map<BaseType_t, uint32_t>* recordings)
+        static bool GetCpuTime(std::map<BaseType_t, uint32_t>& outRecordings)
         {
-            // Get idle time for both CPU cores
+            #if configUSE_TRACE_FACILITY == 1
+            //Get idle time for all CPU cores.
             UBaseType_t arraySize = uxTaskGetNumberOfTasks();
             TaskStatus_t* tasksArray = (TaskStatus_t*)malloc(arraySize * sizeof(TaskStatus_t));
 
@@ -30,13 +30,14 @@ namespace ReadieFur::EspGps
             arraySize = uxTaskGetSystemState(tasksArray, arraySize, NULL);
             for (int i = 0; i < arraySize; i++)
                 if (strcmp(tasksArray[i].pcTaskName, "IDLE") == 0)
-                    (*recordings)[tasksArray[i].xTaskNumber] = tasksArray[i].ulRunTimeCounter;
+                    outRecordings[tasksArray[i].xTaskNumber] = tasksArray[i].ulRunTimeCounter;
 
             free(tasksArray);
-
             return true;
+            #else
+            return false;
+            #endif
         }
-        #endif
 
         static void GetFreeMemory(size_t& outIram, size_t& outDram)
         {
@@ -44,33 +45,65 @@ namespace ReadieFur::EspGps
             outDram = heap_caps_get_free_size(MALLOC_CAP_8BIT);
         }
 
+        static bool GetTasksFreeStack(std::map<const char*, size_t>& outRecordings)
+        {
+            #if configUSE_TRACE_FACILITY == 1
+            UBaseType_t arraySize = uxTaskGetNumberOfTasks();
+            TaskStatus_t* tasksArray = (TaskStatus_t*)malloc(arraySize * sizeof(TaskStatus_t));
+
+            if (tasksArray == nullptr)
+                return false;
+
+            //Get system state (task status).
+            UBaseType_t totalTasks = uxTaskGetSystemState(tasksArray, arraySize, NULL);
+
+            //Print free stack space for each task.
+            for (UBaseType_t i = 0; i < totalTasks; i++)
+                outRecordings[tasksArray[i].pcTaskName] = tasksArray[i].usStackHighWaterMark * sizeof(StackType_t);
+
+            //Free allocated memory for taskStatusArray.
+            free(tasksArray);
+            #else
+            return false;
+            #endif
+        }
+
     protected:
         void RunServiceImpl() override
         {
             while (!ServiceCancellationToken.IsCancellationRequested())
             {
-                #if configUSE_TRACE_FACILITY == 1
-                std::map<BaseType_t, uint32_t> cpuRecordings;
-                GetCpuTime(&cpuRecordings);
-                String cpuLogString;
-                for (auto &&recording : cpuRecordings)
-                {
-                    cpuLogString += "CPU";
-                    cpuLogString += recording.first;
-                    cpuLogString += ": ";
-                    cpuLogString += recording.second;
-                    cpuLogString += ", ";
-                }
-                if (cpuLogString.endsWith(", "))
-                    cpuLogString = cpuLogString.substring(0, cpuLogString.length() - 2);
-                cpuRecordings.clear();
-                ESP_LOGD(nameof(Diagnostics), cpuLogString.c_str());
-                cpuLogString.clear();
-                #endif
+                // std::map<BaseType_t, uint32_t> cpuRecordings;
+                // if (!GetCpuTime(cpuRecordings))
+                // {
+                //     String cpuLogString;
+                //     for (auto &&recording : cpuRecordings)
+                //     {
+                //         cpuLogString += "CPU";
+                //         cpuLogString += recording.first;
+                //         cpuLogString += ": ";
+                //         cpuLogString += recording.second;
+                //         cpuLogString += ", ";
+                //     }
+                //     if (cpuLogString.endsWith(", "))
+                //         cpuLogString = cpuLogString.substring(0, cpuLogString.length() - 2);
+                //     cpuRecordings.clear();
+                //     ESP_LOGD(nameof(Diagnostics), "%s", cpuLogString.c_str());
+                //     cpuLogString.clear();
+                // }
 
                 size_t iram, dram;
                 GetFreeMemory(iram, dram);
                 ESP_LOGD(nameof(Diagnostics), "Memory free: IRAM: %u, DRAM: %u", iram, dram);
+
+                // std::map<const char*, size_t> taskRecordings;
+                // if (!GetTasksFreeStack(taskRecordings))
+                // {
+                //     String tasksLogString;
+                //     for (auto &&recording : taskRecordings)
+                //     {
+                //     }
+                // }
 
                 vTaskDelay(pdMS_TO_TICKS(5 * 1000));
             }
@@ -79,7 +112,7 @@ namespace ReadieFur::EspGps
     public:
         Diagnostics()
         {
-            ServiceEntrypointStackDepth += 256;
+            ServiceEntrypointStackDepth += 512;
         }
     };
 };
