7c7bc9aea346c8fb3484d8ab5c42d5fa6bedaffd
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-10-22 11:47:57 +0100

    Update MQTT.hpp

--- src/MQTT.hpp
+++ src/MQTT.hpp
@@ -14,10 +14,12 @@ namespace ReadieFur::EspGps
     class MQTT : public Service::AService
     {
     private:
+        GSM* _gsmService = nullptr;
         TinyGsmClient* _gsmClient = nullptr;
         PubSubClient _mqtt;
         String _publishTopic;
         String _subscribeTopic;
+        bool _wasConnected = false;
 
         static void Callback(char* topic, byte* payload, uint len)
         {
@@ -26,12 +28,23 @@ namespace ReadieFur::EspGps
             //TODO: Send to API.
         }
 
-        bool Connect()
+        bool ValidateConnection()
         {
-            if (_mqtt.connected())
+            bool gsmConnected = _gsmService->IsConnected();
+            if (gsmConnected && _mqtt.connected())
                 return true;
 
-            ESP_LOGW(nameof(MQTT), "Disconnected from MQTT server...");
+            if (_wasConnected)
+            {
+                ESP_LOGW(nameof(MQTT), "Disconnected from MQTT server...");
+                _wasConnected = false;
+            }
+
+            if (!gsmConnected)
+            {
+                //Skip this attempt and try again when the connection is restored.
+                return false;
+            }
 
             //Connect to MQTT broker.
             if (!_mqtt.connect(GetConfig(const char*, MQTT_CLIENT_ID),
@@ -44,17 +57,17 @@ namespace ReadieFur::EspGps
             _mqtt.subscribe(_subscribeTopic.c_str());
 
             ESP_LOGI(nameof(MQTT), "MQTT reconnected.");
-            return _mqtt.connected();
+            return _wasConnected = true;
         }
 
     protected:
         void RunServiceImpl() override
         {
             //Shouldn't be null here.
-            GSM* gsmService = GetService<GSM>();
-
-            _gsmClient = gsmService->CreateClient();
-            if (_gsmClient == nullptr);
+            _gsmService = GetService<GSM>();
+            _gsmService->WaitForConnection();
+            _gsmClient = _gsmService->CreateClient();
+            if (_gsmClient == nullptr)
             {
                 ESP_LOGE(nameof(MQTT), "Failed to create client.");
                 abort();
@@ -62,24 +75,20 @@ namespace ReadieFur::EspGps
             }
 
             _mqtt.setClient(*_gsmClient);
-
-            _subscribeTopic = GetConfig(String, MQTT_TOPIC) + "/" + GetConfig(String, MQTT_CLIENT_ID) + "/api";
-            _publishTopic = GetConfig(String, MQTT_TOPIC) + "/" + GetConfig(String, MQTT_CLIENT_ID) + "/data";
-
-            //MQTT broker setup.
             _mqtt.setServer(GetConfig(const char*, MQTT_BROKER), GetConfig(int, MQTT_PORT));
             _mqtt.setCallback(Callback);
 
             while (!ServiceCancellationToken.IsCancellationRequested())
             {
-                Connect();
-                _mqtt.loop();
+                if (ValidateConnection())
+                    _mqtt.loop();
                 vTaskDelay(pdMS_TO_TICKS(1000));
             }
 
             _mqtt.disconnect();
-
-            gsmService->DestroyClient(_gsmClient);
+            _gsmService->DestroyClient(_gsmClient);
+            _gsmClient = nullptr;
+            _gsmService = nullptr;
         }
 
     public:
@@ -87,6 +96,8 @@ namespace ReadieFur::EspGps
         {
             ServiceEntrypointStackDepth += 1024;
             AddDependencyType<GSM>();
+            _subscribeTopic = GetConfig(String, MQTT_TOPIC) + "/" + GetConfig(String, MQTT_CLIENT_ID) + "/api";
+            _publishTopic = GetConfig(String, MQTT_TOPIC) + "/" + GetConfig(String, MQTT_CLIENT_ID) + "/data";
         }
 
         const char* GetPublishTopic()
