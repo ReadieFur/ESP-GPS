3a5e2fd5af315976c6649cfc4eea814fbcd69cca
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-10-22 11:47:53 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,42 @@
 #include "Battery.hpp"
 #include "Diagnostic/DiagnosticsService.hpp"
 #include "MQTT.hpp"
+#include <esp_check.h>
+
+#define CHECK_SERVICE_RESULT(func) do {                                     \
+        Service::EServiceResult result = func;                              \
+        if (result == Service::Ok) break;                                   \
+        ESP_LOGE(pcTaskGetName(NULL), "Failed with result: %i", result);    \
+        abort();                                                            \
+    } while (0)
 
 using namespace ReadieFur;
 
 void setup()
 {
-    Service::ServiceManager::InstallService<EspGps::SerialMonitor>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<EspGps::SerialMonitor>());
     #ifdef DEBUG
-    Service::ServiceManager::InstallService<Diagnostic::DiagnosticsService>();
-    Service::ServiceManager::StartService<Diagnostic::DiagnosticsService>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<Diagnostic::DiagnosticsService>());
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<Diagnostic::DiagnosticsService>());
     #endif
 
     EspGps::Storage::Init();
     EspGps::Motion::Configure();
 
     #ifdef BATTERY_ADC
-    Service::ServiceManager::InstallService<EspGps::Battery>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<EspGps::Battery>());
     #endif
-    Service::ServiceManager::InstallService<EspGps::GPS>();
-    Service::ServiceManager::InstallService<EspGps::GSM>();
-    Service::ServiceManager::InstallService<EspGps::MQTT>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<EspGps::GPS>());
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<EspGps::GSM>());
+    CHECK_SERVICE_RESULT(Service::ServiceManager::InstallService<EspGps::MQTT>());
 
-    Service::ServiceManager::StartService<EspGps::SerialMonitor>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<EspGps::SerialMonitor>());
     #ifdef BATTERY_ADC
-    Service::ServiceManager::StartService<EspGps::Battery>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<EspGps::Battery>());
     #endif
-    Service::ServiceManager::StartService<EspGps::GPS>();
-    Service::ServiceManager::StartService<EspGps::GSM>();
-    Service::ServiceManager::StartService<EspGps::MQTT>();
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<EspGps::GPS>());
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<EspGps::GSM>());
+    CHECK_SERVICE_RESULT(Service::ServiceManager::StartService<EspGps::MQTT>());
 }
 
 void loop()
